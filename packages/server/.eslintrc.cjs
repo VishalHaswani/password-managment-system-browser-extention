14195e070aed27ea934d869c209f53b4d196b849
Necrocryosis <53443991+Necrocryosis@users.noreply.github.com>
2023-01-25 01:56:09 +0530

    Created express server using Typescript (#7)
    * Server created additional dependencies installed
    
    * fixed some lint issues
    
    Co-authored-by: VishalHaswani <vishal.haswani2019@vitstudent.ac.in>

---
+++ packages/server/.eslintrc.cjs
@@ -0,0 +1,28 @@
+/* eslint-env node */
+module.exports = {
+  env: {
+    node: true,
+    es2022: true,
+  },
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:n/recommended',
+  ],
+  ignorePatterns: ['build/*'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.eslint.json'],
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    // Custom rules here
+  },
+};
